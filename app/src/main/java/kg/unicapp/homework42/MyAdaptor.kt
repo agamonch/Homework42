e85594a939db83a7206d4be8f040096a389d4d27
agamonch <jnkyrgyz@gmail.com>
2021-06-17 16:06:28 +0300

    init new project home42

---
+++ app/src/main/java/kg/unicapp/homework42/MyAdaptor.kt
@@ -0,0 +1,18 @@
+package kg.unicapp.homework42
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdaptor: RecyclerView.Adapter<MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/kg/unicapp/homework42/MyViewHolder.kt
@@ -0,0 +1,7 @@
+package kg.unicapp.homework42
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+}
